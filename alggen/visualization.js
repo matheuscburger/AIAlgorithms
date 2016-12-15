359e9c64646bf9137fdced108f73d0893d19a2b2
Matheus Carvalho Bürger <matheus.cburger@gmail.com>
2016-12-15 03:58:51 +0000

    Changes in visualization

--- alggen/visualization.js
+++ alggen/visualization.js
@@ -18,9 +18,9 @@ function run_genalg(){
     timer = setInterval(function() {
         genalg.nextgen();
         updateInfo();
-		if(!run){
-		    clearInterval(timer);
-		}
+        if(!run){
+            clearInterval(timer);
+        }
     }, timeout);
 }
 
@@ -29,16 +29,32 @@ $('#next').click(function(){ run_genalg(); return false; });
 $('#run').click(function(){ run = true; run_genalg(); return false; });
 
 function chromosome2divs(chr){
+    var signal = chr.chrom.slice(0, 1).join(" ");
+    var exp = chr.chrom.slice(1,9).join(" ");
+    var mantissa = chr.chrom.slice(9).join(" ");
+    var float = binary2float(chr.chrom);
+    if(Math.abs(float) > 10000){
+        float = float.toFixed(0);
+    } else {
+        float = float.toFixed(5);
+    } 
     res = "<div class='individual'>";
-    res += "<div class='binary'>" + chr.chrom.join(" ") + "</div>";
-    res += "<div class='decimal'>" + binary2float(chr.chrom).toFixed(5) + "</div>";
+    res += "<div class='binary'>"; 
+    res += "<span class='signal'>" + signal + "</span> ";
+    res += "<span class='exponent'>" + exp + "</span> ";
+    res += "<span class='mantissa'>" + mantissa + "</span>";
+    res += "</div>";
+    res += "<div class='decimal'>" + float + "</div>";
     res += "</div>";
     return(res);
 }
 
 function allchromosomes(){
     ind = genalg.individues;
-    res = "";
+    res = "<div class='individual'>";
+    res += "<div class='binary' id='binary_header'>Binary (IEEE 754)</div>";
+    res += "<div class='decimal' id='decimal_header'>Decimal</div>";
+    res += "</div>";
     for(var i=0; i < ind.length; i++){
         res += chromosome2divs(ind[i]);
     }
