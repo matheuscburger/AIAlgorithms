1bde1cd7a06138857f99389024e5791ba51c6303
Matheus <matheus.cburger@gmail.com>
2017-01-24 23:04:02 -0200

    using logistic function

--- perceptron/perceptron.ts
+++ perceptron/perceptron.ts
@@ -15,8 +15,11 @@ function inner_product (x:Array<number>, y:Array<number>):number {
 
 
 class Perceptron {
-    weights:  Array<number>;
+    public weights:  Array<number>;
+    public learning_rate: number;
+
     constructor(len:number){
+        this.learning_rate = 0.9;
         this.weights = new Array(len+1);
         for(let i:number = 0; i <= len; i++){
             this.weights[i] = 0;
@@ -24,8 +27,15 @@ class Perceptron {
     }
 
     public activation (x:number):number {
-      let res:number = (x >= 0)?1:0;
-      return(res);
+        let res:number;
+        res=1/(1 + Math.exp(-x)); // logistic function
+        return(res);
+    }
+
+    public gradient (x:number):number {
+        let log:number = this.activation(x);
+        let res:number = log*(1-log); // logistic derivative
+        return(res)
     }
 
     public update_weights(features:Array<number>, real_output:number):void{
@@ -33,17 +43,34 @@ class Perceptron {
         feat.unshift(1); // add bias
         let new_w:Array<number> = this.weights.slice(0);
         let output:number = this.predict(features);
+        let error:number = (real_output - output);
+        let grad:number = this.gradient(inner_product(feat, this.weights));
         for(let i:number=0; i < this.weights.length; i++){
-            new_w[i] = this.weights[i] + (real_output - output) * feat[i];
+            new_w[i] = this.weights[i] + this.learning_rate * error * grad * feat[i];
         }
         this.weights = new_w;
     }
 
     public train_one_epoch(arr_features:Array<Array<number>>,
       outputs:Array<number>): void {
-        console.log(this.weights);
         for(let i:number=0; i < outputs.length; i++){
             this.update_weights(arr_features[i], outputs[i]);
+        }
+    }
+
+    public train(arr_features:Array<Array<number>>,
+      outputs:Array<number>, eps=0.01, max_iter=1e6):void{
+        let iter:number = 0;
+        let mean_error:number = 1000;
+        while(mean_error > eps || iter < max_iter){
+            this.train_one_epoch(arr_features, outputs);
+            let sum_error:number = 0;
+            for(let i:number=0; i < arr_features.length; i++){
+                let pred:number = this.predict(arr_features[i]);
+                sum_error += Math.abs(pred-outputs[i]);
+            }
+            mean_error = sum_error/arr_features.length;
+            iter ++;
         }
     }
 
@@ -53,6 +80,7 @@ class Perceptron {
         let res:number = this.activation(inner_product(feat, this.weights));
         return(res);
     }
+
 }
 
 //tests
@@ -67,10 +95,7 @@ let and_data = [[0, 0],
 
 let and_out = [0, 0, 0, 1];
 
-for(let i:number=0; i < 5; i++)
-    percep.train_one_epoch(and_data, and_out);
-
-
+percep.train(and_data, and_out);
 
 console.log(percep.weights);
 
